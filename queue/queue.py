376739182c29dbc1c11697b70c99f9cce07f7870
Tyler Alsop <tylerjalsop@gmail.com>
2020-06-17 20:52:30 -0600

    Attempted queue.py but I am getting a strange error when I try to run the test. I think the issue has to do with the imports from the singly_linked_list file.

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,6 @@
+from singly_linked_list_queue import Node
+from singly_linked_list_queue import LinkedList
+
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
@@ -13,16 +16,41 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+
+
+############ Array Implementation ############
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+    
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         self.storage.append(value)
+
+#     def dequeue(self):
+#         self.storage.pop(0)
+
+
+############ LinkedList Implementation ############
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = LinkedList()
+
     def __len__(self):
-        pass
+        return self.size
 
-    def enqueue(self, value):
-        pass
+    def push(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
 
-    def dequeue(self):
-        pass
+    def pop(self):
+        if self.size == 0:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.remove_head()
--- queue/singly_linked_list_queue.py
+++ queue/singly_linked_list_queue.py
@@ -1,4 +1,3 @@
-
 class Node:
     def __init__(self, value=None, next_node=None):
         self.value = value
@@ -15,37 +14,37 @@ class LinkedList:
         new_node = Node(value)
         # check if list is empty
         if self.head is None and self.tail is None:
-        self.head = new_node
-        self.tail = new_node
+            self.head = new_node
+            self.tail = new_node
         else:
-        # new_node should point to current head
-        new_node.next_node = self.head
-        # move head to new node
-        self.head = new_node
+            # new_node should point to current head
+            new_node.next_node = self.head
+            # move head to new node
+            self.head = new_node
 
     def add_to_tail(self, value):
         # create a node to add
         new_node = Node(value)
         # check if list is empty
         if self.head is None and self.tail is None:
-        self.head = new_node
-        self.tail = new_node
+            self.head = new_node
+            self.tail = new_node
         else:
-        # point the node at the current tail, to the new node
-        self.tail.next_node = new_node
-        self.tail = new_node
+            # point the node at the current tail, to the new node
+            self.tail.next_node = new_node
+            self.tail = new_node
 
     # remove the head and return its value
     def remove_head(self):
         # if list is empty, do nothing
         if not self.head:
-        return None
+            return None
         # if list only has one element, set head and tail to None
         if self.head.next_node is None:
-        head_value = self.head.value
-        self.head = None
-        self.tail = None
-        return head_value
+            head_value = self.head.value
+            self.head = None
+            self.tail = None
+            return head_value
         # otherwise we have more elements in the list
         head_value = self.head.value
         self.head = self.head.next_node
@@ -53,19 +52,22 @@ class LinkedList:
 
     def contains(self, value):
         if self.head is None:
-        return False
+            return False
         
         # Loop through each node, until we see the value, or we cannot go further
         current_node = self.head
 
         while current_node is not None:
         # check if this is the node we are looking for
-        if current_node.value == value:
-            return True
+            if current_node.value == value:
+                return True
 
         # otherwise, go to the next node
-        current_node = current_node.next_node
+            current_node = current_node.next_node
         return False 
+    
+    def remove_tail(self):
+
 
   
 
