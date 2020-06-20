7c29c76371b0f2d24fe5605aa4dcb323985da2be
Tyler Alsop <tylerjalsop@gmail.com>
2020-06-19 19:08:36 -0600

    Coded move_to_front. Test passing.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -127,7 +127,19 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            pass
+        # node_value = node.value
+        # node.delete()
+        # self.add_to_head(node_value)
+
+        node.delete()
+        old_head = self.head
+        self.head.prev = node
+        node.next = self.head
+        self.head = node
+        node.prev = None
+        return old_head
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
